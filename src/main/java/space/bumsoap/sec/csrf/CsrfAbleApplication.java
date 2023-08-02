4a29dd4b8ac74c7596c00d5c509e3d55c5f70b57
osparking <jbpark03@gmail.com>
2023-08-02 16:17:23 +0900

    initial w/ starter-security only

---
+++ src/main/java/space/bumsoap/sec/csrf/CsrfAbleApplication.java
@@ -0,0 +1,13 @@
+package space.bumsoap.sec.csrf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsrfAbleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsrfAbleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/space/bumsoap/sec/csrf/CsrfAbleApplicationTests.java
@@ -0,0 +1,13 @@
+package space.bumsoap.sec.csrf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsrfAbleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
